ec7dbe6ce7df003e53bb48b9dc09a2938ed3669e
Homum <hahsan@conversantmedia.com>
2020-12-11 08:25:42 -0600

    added environment variables

--- backend/services/groupme.js
+++ backend/services/groupme.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const environment = require('dotenv').config();
 
-router.get('/', function(req,res) {
+router.get('/', function(req, res) {
     res.send('GET handler for /groupme route');
 })
+
+router.post('/', function(req, res) {
+    res.send('POST handler for /groupme route.');
+});
+
+console.log('process.env.GROUPME_TOKEN: ', process.env.GROUPME_TOKEN)
 
 module.exports = router;
