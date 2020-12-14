d6a4633361e7b15df770270b16162a2e4d5a67bc
Homum <hahsan@conversantmedia.com>
2020-12-14 10:17:04 -0600

    got an api call sending back a valid response

--- backend/services/groupme.js
+++ backend/services/groupme.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+const baseUrl = 'https://api.groupme.com/v3';
+const token = process.env.GROUPME_TOKEN;
+
 router.get('/', function(req, res) {
     res.send('GET handler for /groupme route');
 })
@@ -10,6 +13,24 @@ router.post('/', function(req, res) {
     res.send('POST handler for /groupme route.');
 });
 
-console.log('process.env.GROUPME_TOKEN: ', process.env.GROUPME_TOKEN)
+router.get(`${this.baseUrl}/groups`, (req, res) => {
+    console.log(res);
+});
 
-module.exports = router;
+
+var config = {
+  method: 'get',
+  url: 'https://api.groupme.com/v3/groups?token=8a191dc01d4b0139e91636c88cc7aee6',
+  headers: { 
+    'Content-Type': 'application/json'
+  }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
